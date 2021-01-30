636f088dcc31897bb36cb37c7f86814a5de49c44
Tom Vempala <54186859+tomthomasvempala@users.noreply.github.com>
2021-01-30 14:13:45 +0530

    added main screen and nav bar

---
+++ lib/Pages/MainScreen/mainScreen.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+
+import '../../size_config.dart';
+import '../../themeData.dart';
+
+class MainScreen extends StatefulWidget {
+  @override
+  _MainScreenState createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int _currentIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    var navBarItems = [
+      {
+        'title': 'Home',
+        'icon': Icon(
+          _currentIndex == 0 ? Icons.home_filled : Icons.home_outlined,
+        )
+      },
+      {
+        'title': 'Explore',
+        'icon':
+            Icon(_currentIndex == 1 ? Icons.explore : Icons.explore_outlined)
+      },
+      {
+        'title': 'DM',
+        'icon':
+            Icon(_currentIndex == 2 ? Icons.message : Icons.message_outlined)
+      },
+      {
+        'title': 'Account',
+        'icon': Icon(_currentIndex == 3
+            ? Icons.emoji_emotions
+            : Icons.emoji_emotions_outlined)
+      }
+    ];
+    return Scaffold(
+        bottomNavigationBar: SizedBox(
+      // height: (80 / 9.48) * SizeConfig.heightMultiplier,
+      child: BottomNavigationBar(
+        iconSize: 6.5 * SizeConfig.widthMultiplier,
+        backgroundColor: Theme.of(context).canvasColor,
+        type: BottomNavigationBarType.fixed,
+        selectedItemColor: AppTheme.darkBlue,
+        unselectedItemColor: AppTheme.darkBlue,
+        unselectedFontSize: 3.5 * SizeConfig.textMultiplier,
+        selectedFontSize: 4 * SizeConfig.textMultiplier,
+        currentIndex: _currentIndex,
+        items: navBarItems
+            .map(
+              (e) => BottomNavigationBarItem(
+                icon: e['icon'],
+                label: e['title'],
+              ),
+            )
+            .toList(),
+        onTap: onTaped,
+      ),
+    ));
+  }
+
+  void onTaped(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+}
---
+++ lib/themeData.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static final Color darkBlue = Color.fromRGBO(0, 0, 52, 1);
+  static final Color blue = Color.fromRGBO(45, 62, 224, 1);
+  static final Color green = Color.fromRGBO(36, 243, 195, 1);
+  static final Color grey = Color.fromRGBO(242, 245, 248, 1);
+  static final Color white = Color.fromRGBO(255, 255, 255, 1);
+}
